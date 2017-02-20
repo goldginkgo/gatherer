75ee3f12b8cebf7d121a21d932191f963d44e7c9
Frank Dai <yongjindai09@gmail.com>
2017-02-21 12:52:35 +1300

    test first rspec

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,15 +1,11 @@
-class Project
+class Project < ActiveRecord::Base
 
-  attr_accessor :tasks, :due_date
+  has_many :tasks
 
-#
+  validates :name, presence: true
+
   def self.velocity_length_in_days
     21
-  end
-#
-
-  def initialize
-    @tasks = []
   end
 
   def incomplete_tasks
@@ -21,7 +17,7 @@ class Project
   end
 
   def total_size
-    tasks.sum(&:size)
+    tasks.to_a.sum(&:size)
   end
 
   def remaining_size
@@ -30,23 +26,19 @@ class Project
 
 
   def completed_velocity
-    tasks.sum(&:points_toward_velocity)
+    tasks.to_a.sum(&:points_toward_velocity)
   end
 
-  #
   def current_rate
     completed_velocity * 1.0 / Project.velocity_length_in_days
   end
-  #
 
   def projected_days_remaining
     remaining_size / current_rate
   end
 
-  #
   def on_schedule?
     return false if projected_days_remaining.nan?
     (Date.today + projected_days_remaining) <= due_date
   end
-  #
 end
